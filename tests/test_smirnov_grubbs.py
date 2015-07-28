9895a130236a7248a6afa311c2e761aec9fab124
c-bata <contact@c-bata.link>
2015-07-28 15:47:17 +0900

    Add test case

--- tests/test_smirnov_grubbs.py
+++ tests/test_smirnov_grubbs.py
@@ -28,6 +28,18 @@ class SmirnovGrubbsTests(TestCase):
         actual_index = grubbs._get_target_index(dataset)
         self.assertEqual(actual_index, expected_index)
 
+    def test_test_once_when_given_series(self):
+        data = pd.Series([0, 10, 10, 10])
+        actual_index = grubbs._test_once(data, 0.05)
+        expected_index = 0
+        self.assertEqual(actual_index, expected_index)
+
+    def test_test_once_when_given_numpy_ndarray(self):
+        data = np.array([0, 10, 10, 10])
+        actual_index = grubbs._test_once(data, 0.05)
+        expected_index = 0
+        self.assertEqual(actual_index, expected_index)
+
     def test_delete_item_when_given_series(self):
         data = pd.Series([0, 1, 2, 3])
         actual_data = grubbs._delete_item(data, 1)
