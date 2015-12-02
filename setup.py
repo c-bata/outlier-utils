04e3d3e62b229d87679ea75577cf577457713c3f
c-bata <contact@c-bata.link>
2015-12-02 10:48:35 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,25 +1,42 @@
 import os
-from setuptools import setup
-
-__author__ = 'Masashi Shibata <contact@c-bata.link>'
-__version__ = '0.0.0'
+from setuptools import setup, find_packages
 
 BASE_PATH = os.path.abspath(os.path.dirname(__file__))
 README = open(os.path.join(BASE_PATH, 'README.rst')).read()
 CHANGES = open(os.path.join(BASE_PATH, 'CHANGES.rst')).read()
 
+__version__ = '0.0.1'
+__author__ = 'Masashi Shibata <contact@c-bata.link>'
+__author_email__ = 'contact@c-bata.link'
+__license__ = 'MIT License'
+
+__classifiers__ = [
+    'Development Status :: 3 - Alpha',
+    'Environment :: Console',
+    'Intended Audience :: Science/Research',
+    'Topic :: Scientific/Engineering',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python',
+    'Programming Language :: Python :: 2',
+    'Programming Language :: Python :: 3',
+    'Programming Language :: Python :: 2.7',
+    'Programming Language :: Python :: 3.3',
+    'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3.5',
+]
+
 setup(
     name='outlier_utils',
     version=__version__,
     author=__author__,
-    author_email='contact@c-bata.link',
+    author_email=__author_email__,
     url='https://github.com/c-bata/outlier-utils',
     description='Remove outliers with some methods',
     long_description=README + '\n\n' + CHANGES,
-    package=['outliers'],
+    packages=find_packages(exclude=['test*']),
     install_requirements=['numpy', 'scipy', 'pandas'],
-    keywords=['outlier', 'grubbs', 'pandas', 'numpy'],
-    license='MIT License',
+    keywords='outlier grubbs pandas numpy',
+    license=__license__,
     include_package_data=True,
     test_suite='tests',
 )
