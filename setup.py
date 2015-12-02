4b2a146924b9280165ccbfd0f8b75b7f18bc2ce2
c-bata <contact@c-bata.link>
2015-12-02 11:01:20 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,17 +1,19 @@
+import sys
 import os
 from setuptools import setup, find_packages
+from setuptools.command.test import test as TestCommand
 
 BASE_PATH = os.path.abspath(os.path.dirname(__file__))
 README = open(os.path.join(BASE_PATH, 'README.rst')).read()
 CHANGES = open(os.path.join(BASE_PATH, 'CHANGES.rst')).read()
 
-__version__ = '0.0.1'
+__version__ = '0.0.2'
 __author__ = 'Masashi Shibata <contact@c-bata.link>'
 __author_email__ = 'contact@c-bata.link'
 __license__ = 'MIT License'
 
 __classifiers__ = [
-    'Development Status :: 3 - Alpha',
+    'Development Status :: 1 - Beta',
     'Environment :: Console',
     'Intended Audience :: Science/Research',
     'Topic :: Scientific/Engineering',
@@ -24,6 +26,26 @@ __classifiers__ = [
     'Programming Language :: Python :: 3.4',
     'Programming Language :: Python :: 3.5',
 ]
+
+
+class PyTest(TestCommand):
+    user_options = [('pytest-args=', 'a', "Arguments to pass to py.test")]
+
+    def initialize_options(self):
+        TestCommand.initialize_options(self)
+        self.pytest_args = []
+
+    def finalize_options(self):
+        TestCommand.finalize_options(self)
+        self.test_args = []
+        self.test_suite = True
+
+    def run_tests(self):
+        # import here, cause outside the eggs aren't loaded
+        import pytest
+        errno = pytest.main(self.pytest_args)
+        sys.exit(errno)
+
 
 setup(
     name='outlier_utils',
@@ -39,4 +61,6 @@ setup(
     license=__license__,
     include_package_data=True,
     test_suite='tests',
+    tests_require=['pytest'],
+    cmdclass={'test': PyTest},
 )
